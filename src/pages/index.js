61cdf6b389b6b0f486b5df7e6c7c32abb19478da
TawalMc <developtawaliou@gmail.com>
2021-09-16 18:43:09 +0100

    add head

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="An easy way to manipulate EEPROM data on electronic boards">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
